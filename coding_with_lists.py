eb0e1c40dfa5c51ab367493287a961eedd36b7a7
advanceplays <adhyayanw.24277@obhs.school.nz>
2025-07-28 12:15:04 +1200

    added library

---
+++ coding_with_lists.py
@@ -0,0 +1,19 @@
+'''
+    author: Adhyayan Wagle
+    date: 28/07/25
+    version: 1
+    description: We will be making lists
+
+'''
+
+import random
+
+name = input('Enetr your name:' )
+age = int(input('Enter your age: '))
+
+name = []
+
+for i in range(1,5):
+    print(i)
+
+
